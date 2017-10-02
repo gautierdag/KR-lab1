c6db9774f6cefd811f04e6af2b8da0c746552957
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2017-10-02 13:44:37 +0200

    readme edit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # KR-lab1
 Knowledge Representation - UvA - lab1
+Authors: Juan Buhagiar Gautier Dagan
+
+##Details:
+
+This is our repo for the 2017 KR class - Sudoku Lab.
+To run experiment, please edit the MAIN variables in main.py ()
+and run file.
+
+Please keep in mind that choosing a high number of sudokus and low % interval will affect the time
+considerably.
+
+Other files include:
+  - scraper.py (scrapes menneske.no for 2b2, 3b3, 4b4, and 5b5 sudokus).
+  - solve-script.py (python script called in subprocess with temporary cnf file)
+  - sudokusolver.py (python file containing logic for the encoding the sudokus and calling the subprocess)
+  - readSudokus.py (python file containing logic to read in sudoku.txt files obtained from scraper)
+  - .CSV files are results from the experiments that we obtained
+  - results.xlxs is an excel file with the combined statistics for all sizes.
--- main.py
+++ main.py
@@ -16,45 +16,101 @@ from sudokusolver import solve
 from readSudokus import parse, removePercentage
 import matplotlib.pyplot as plt
 
+NUMBER_OF_SUDOKUS_PER_SIZE = 5
+KNOWN_PERCENTAGE_INTERVAL = 0.1
+#Show graph results of all runs
+SHOW_GRAPH = True
+#save results to csv - we ran each size separately for the experiment
+# so this is not set up to save results for all sizes
+SAVE_RESULTS = False
+
 def main():
     pp = pprint.PrettyPrinter(indent=4)
     currentDir = os.path.realpath(__file__);
     dir_path = join(os.path.dirname(currentDir), "puzzles")
     dirs = [d for d in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, d))]
 
-    percentages = np.arange(0.2, 1, 0.1) #the percentages of known numbers on the grid
+    percentages = np.arange(0.2, 1, KNOWN_PERCENTAGE_INTERVAL) #the percentages of known numbers on the grid
     ALL_STATS = np.zeros((4, len(percentages), 10))
     sdk_type_index = 0
-    count = 0
+
+    if NUMBER_OF_SUDOKUS_PER_SIZE > 4000:
+        print("Number of Sudokus out of range, select <= 4000")
+        return
     # print(dirs)
-    for sudokuType in [dirs[3]]:
+    for sudokuType in dirs:
+        print("Processing sudokus of size : {0}".format(sudokuType))
         current_path = join(dir_path, sudokuType)
         onlyfiles = [f for f in listdir(current_path) if isfile(join(current_path, f))];
         TYPE_STATS = np.zeros((len(percentages), 10))
-        total = len(onlyfiles[-1000:])
-        if count == 0:
-            for file in onlyfiles[-1000:]:
-                temp_STATS = np.zeros((len(percentages), 10))
-                for p in range(len(percentages)):
-                    sudoku = parse(current_path, file, int(sudokuType))
-                    # pp.pprint(sudoku)
-                    sudokuWith0 = removePercentage(sudoku, (1-percentages[p]), int(sudokuType))
-                    # pp.pprint(sudokuWith0)
-                    temp_STATS[p] = solve(sudokuWith0)
-                    # pp.pprint(sudokuWith0)
+        total = len(onlyfiles[-NUMBER_OF_SUDOKUS_PER_SIZE:])
+        count = 0
+        for file in onlyfiles[-NUMBER_OF_SUDOKUS_PER_SIZE:]:
+            temp_STATS = np.zeros((len(percentages), 10))
+            for p in range(len(percentages)):
+                sudoku = parse(current_path, file, int(sudokuType))
+                # pp.pprint(sudoku)
+                sudokuWith0 = removePercentage(sudoku, (1-percentages[p]), int(sudokuType))
+                # pp.pprint(sudokuWith0)
+                temp_STATS[p] = solve(sudokuWith0)
+                # pp.pprint(sudokuWith0)
 
-                TYPE_STATS = (temp_STATS*(1/total))+TYPE_STATS
-                count += 1
-                print("Processed: {0} out of {1}".format(count, total))
-                if count%5 == 0:
-                    print("Saving Current Progress")
-                    with open('results.csv','wb') as f:
-                        np.savetxt(f, TYPE_STATS, delimiter=",")
+            TYPE_STATS = (temp_STATS*(1/total))+TYPE_STATS
+            count += 1
+            print("Processed: {0} out of {1}".format(count, total))
+
+            #saving results while running (note this only will only save results for the current sdk_type_index)
+            if count%5 == 0 and SAVE_RESULTS:
+                print("Saving Current Progress")
+                with open('results.csv','wb') as f:
+                    np.savetxt(f, TYPE_STATS, delimiter=",")
 
             ALL_STATS[sdk_type_index] = TYPE_STATS
-        # sdk_type_index += 1
-    with open('results.csv','wb') as f:
-        np.savetxt(f, ALL_STATS[0], delimiter=",")
+        sdk_type_index += 1
+
+    #Save Results of sdk_type_index:
+    if SAVE_RESULTS
+        with open('results.csv','wb') as f:
+            np.savetxt(f, ALL_STATS[sdk_type_index], delimiter=",")
+
+    #Print the Stats:
+    if SHOW_GRAPH:
+        fig, ax = plt.subplots(5,2, sharex='col')
+
+        ax[0, 0].plot(percentages, ALL_STATS.T[0])
+        ax[0, 0].set_title('Seconds')
+
+        ax[0, 1].plot(percentages, ALL_STATS.T[1])
+        ax[0, 1].set_title('Level')
+
+        ax[1, 1].plot(percentages, ALL_STATS.T[2])
+        ax[1, 1].set_title('Variables')
+
+        ax[2, 1].plot(percentages, ALL_STATS.T[3])
+        ax[2, 1].set_title('Used')
+
+        ax[3, 1].plot(percentages, ALL_STATS.T[4])
+        ax[3, 1].set_title('Original')
+
+        ax[1, 0].plot(percentages, ALL_STATS.T[5])
+        ax[1, 0].set_title('Conflicts')
+
+        ax[2, 0].plot(percentages, ALL_STATS.T[6])
+        ax[2, 0].set_title('Learned')
+
+        ax[3, 0].plot(percentages, ALL_STATS.T[7])
+        ax[3, 0].set_title('Limit')
+
+        ax[4, 0].plot(percentages, ALL_STATS.T[8])
+        ax[4, 0].set_title('Agility')
+
+        ax[4, 1].plot(percentages, ALL_STATS.T[9])
+        ax[4, 1].set_title('MB')
+
+        fig.subplots_adjust(hspace=0.55)
+
+        plt.show()
+
 
 if __name__ == "__main__":
     main()
